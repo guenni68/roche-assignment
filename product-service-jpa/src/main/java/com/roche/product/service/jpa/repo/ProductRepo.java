77198bbd3b80c4d3dff82cd8361f2e79da71eaf7
Guenther Schmidt <schmig68@gmail.com>
2020-08-15 16:11:31 +0100

    changed repo return native

--- product-service-jpa/src/main/java/com/roche/product/service/jpa/repo/ProductRepo.java
+++ product-service-jpa/src/main/java/com/roche/product/service/jpa/repo/ProductRepo.java
@@ -1,6 +1,5 @@
 package com.roche.product.service.jpa.repo;
 
-import com.roche.product.service.api.model.Product;
 import com.roche.product.service.jpa.entity.ProductJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +9,7 @@ import java.util.List;
 @Repository
 public interface ProductRepo extends JpaRepository<ProductJpa, Long> {
 
-    Product findOneByIdAndIsDeletedFalse(Long id);
+    ProductJpa findOneByIdAndIsDeletedFalse(Long id);
 
     List<ProductJpa> findAllByIsDeletedFalse();
 }
